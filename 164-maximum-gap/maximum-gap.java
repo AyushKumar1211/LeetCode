9d8f0be849d4f28c07d5dd0513e0af09120af46f
AyushKumar1211 <145289543+AyushKumar1211@users.noreply.github.com>
2026-07-09 10:17:59 +0530

    Time: 47 ms (17.02%) | Memory: 90.9 MB (55.47%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if(n<=1){
+            return 0;
+        }
+        int max =0;
+        Arrays.sort(nums);
+        for(int i=0;i<n-1;i++){
+            int dif = nums[i+1]-nums[i];
+            if(dif>max){
+                max = dif;
+            }
+        }
+        return max;
+    }
+}
